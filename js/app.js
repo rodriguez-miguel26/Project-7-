1da4ace8b74ff9ffdc592fb0d5c1ac8c4020dc5b
rodriguez-miguel26 <85662329+rodriguez-miguel26@users.noreply.github.com>
2023-04-12 18:45:37 -0700

    Added all charts, need to work on viewport size

--- js/app.js
+++ js/app.js
@@ -56,3 +56,73 @@ let trafficChart = new Chart(trafficCanvas, {
     data: trafficData,
     options: trafficOptions
 });
+
+/*-------- bar chart --------------*/
+
+const dailyCanvas = document.getElementById("daily-chart");
+
+const dailyData = {
+    labels: ["S", "M", "T", "W", "T", "F", "S"],
+    datasets: [{
+        label: '# of Hits',
+        data: [75, 115, 175, 125, 225, 200, 100],
+        backgroundColor: '#7477BF',
+        borderWidth: 1
+    }]
+};
+    const dailyOptions = {
+        scales: {
+        y: {
+            beginAtZero: true
+        }
+    },
+    plugins: {
+        legend: {
+        display: false
+        }
+    }
+ };
+
+ let dailyChart = new Chart(dailyCanvas, {
+    type: 'bar',
+    data: dailyData,
+    options: dailyOptions
+});
+
+/*-------- mobile chart --------------*/
+
+const mobileCanvas = document.getElementById("mobile-chart");
+
+const mobileData= {
+    label: ["Desktop", "Tablet", "Phones"],
+    datasets: [{
+        label: '# of users', 
+        data: [2000, 550, 500],
+        borderWidth: 0,
+        backgroundColor: [
+            '#7477BF',
+            '#78CF82',
+            '#51B6C8'
+        ]
+    }]
+};
+
+const mobileOptions = {
+    aspectRatio: 1.9, 
+    plugins: {
+        legend: {
+            position: 'right',
+            labels: {
+                boxWidth: 20,
+                fontStyle:'bold'
+            }
+        }
+    }            
+};
+
+let mobileChart = new Chart(mobileCanvas, {
+    type: 'doughnut',
+    data: mobileData, 
+    options: mobileOptions
+
+});
