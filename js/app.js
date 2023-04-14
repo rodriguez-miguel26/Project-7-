d138e277d848bbb0af1af9feb42d42cb66838b30
rodriguez-miguel26 <85662329+rodriguez-miguel26@users.noreply.github.com>
2023-04-13 18:13:03 -0700

    all charts added

--- js/app.js
+++ js/app.js
@@ -126,3 +126,22 @@ let mobileChart = new Chart(mobileCanvas, {
     options: mobileOptions
 
 });
+
+/*----------Message form-------------*/
+
+const user = document.getElementById("userField");
+const message = document.getElementById("messageField");
+const send = document.getElementById("send");
+
+send.addEventListener('click', () => {
+    
+    if (user.value=== "" && message.value ==="") {
+        alert("Please fill out user and message fields before sending");
+    }   else if (user.value === "") {
+        alert("Please fill out user field before sending");
+    }   else if (message.value === ""){
+        alert("Please fill out message filed befrore sending");
+    }   else {
+        alert(`Message successfully sent to: ${user.value}`);
+    }
+});
